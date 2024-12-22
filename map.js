37ae3247d90a81f825640f0ca03a5189adc9f76d
haripriya gullipalli <183710614+haripriyagullipalli@users.noreply.github.com>
2024-12-22 23:02:52 +0530

    added upperCase function

--- map.js
+++ map.js
@@ -16,10 +16,17 @@ const lengthsOf = function (strings) {
 };
 
 console.log(lengthsOf(["apple", "banana", "kiwi"]));
-console.log = function () { }
+// console.log = function () { }
 
 // uppercase of ["hello", "world"] => ["HELLO", "WORLD"]
-const uppercaseOf = function (strings) { };
+const uppercaseOf = function (strings) {
+  return strings.map(function (string) {
+    return string.toUpperCase();
+  })
+};
+
+console.log(uppercaseOf(["hello", "world"]));
+console.log = function () { }
 
 // first characters of ["apple", "banana", "kiwi"] => ["a", "b", "k"]
 const firstCharactersOf = function (strings) { };
